8510e9e3bcbbff6b6abcd928b1269be676ec0f64
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-03-02 11:36:26 +0530

    Add base domain classes and enhance validation logic
    Introduce `EntityBase` and `ValueObject` abstractions to standardize domain modeling and value equality in the domain layer. Refactor `ValidationBehaviour` to streamline conditional checks and improve readability in validation handling.

--- Alstom/SpectrailTestDataProvider.Application/Behaviours/ValidationBehaviour.cs
+++ Alstom/SpectrailTestDataProvider.Application/Behaviours/ValidationBehaviour.cs
@@ -12,19 +12,16 @@ public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRe
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        if (_validators.Any())
+        if (!_validators.Any()) return await next();
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failure = validationResult.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+        if (failure.Count!=0)
         {
-            var context = new ValidationContext<TRequest>(request);
-
-            var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-
-            var failure = validationResult.SelectMany(r => r.Errors).Where(f => f != null).ToList();
-
-            if (failure.Count!=0)
-            {
-                throw new ValidationException(failure);
-            }
-
+            throw new ValidationException(failure);
         }
         return await next();
     }
---
+++ Alstom/SpectrailTestDataProvider.Domain/Entities/ICDCustomColumn.cs
@@ -0,0 +1,8 @@
+using SpectrailTestDataProvider.Domain.Common;
+
+namespace SpectrailTestDataProvider.Domain.Entities;
+
+public class ICDCustomColumn:EntityBase
+{
+    
+}
