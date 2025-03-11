93210587da737e9eb67ecf7b490726c9dadfe743
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-03-11 23:10:17 +0530

    Refactor service registration and improve readability.
    Replaced `Assembly.GetExecutingAssembly()` with `GetExecutingAssembly()` for clarity and inline static usage. Adjusted line formatting for better code readability and consistency, particularly in scoped service registrations. Removed unused `System.Reflection` import to clean up dependencies.

--- Alstom/SpectrailTestServer/SpectrailTestDataProvider.Application/RegisterApplicationServices.cs
+++ Alstom/SpectrailTestServer/SpectrailTestDataProvider.Application/RegisterApplicationServices.cs
@@ -1,6 +1,5 @@
 #region
 
-using System.Reflection;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,8 +7,8 @@ using SpectrailTestDataProvider.Application.Behaviours;
 using SpectrailTestDataProvider.Application.Contracts;
 using SpectrailTestDataProvider.Application.Features.ICD.Queries.Handler;
 using SpectrailTestDataProvider.Application.Features.ICD.Queries.Query;
-using SpectrailTestDataProvider.Application.Features.ICD.Services;
 using SpectrailTestDataProvider.Domain.Entities.ICD;
+using static System.Reflection.Assembly;
 
 #endregion
 
@@ -19,12 +18,15 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
     {
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());
-        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        // ReSharper disable once HeapView.ObjectAllocation
+        services.AddAutoMapper(GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(GetExecutingAssembly());
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetRepositoryQueryHandler<>).Assembly));
-        services.AddScoped<IRequestHandler<GetRepositoryQuery<DCUEntity>, IAsyncRepository<DCUEntity>>, GetRepositoryQueryHandler<DCUEntity>>();
+        services
+            .AddScoped<IRequestHandler<GetRepositoryQuery<DCUEntity>, IAsyncRepository<DCUEntity>>,
+                GetRepositoryQueryHandler<DCUEntity>>();
         return services;
     }
 }
