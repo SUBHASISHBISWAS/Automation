c4e72dd102e809922064367f9039df4e194c2d16
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-03-13 20:05:21 +0530

    Add copyright headers to all source files
    Included Alstom's proprietary and confidential copyright notice across all files in the TestFramework project. This ensures legal compliance and uniform documentation throughout the codebase.

--- Alstom/SpectrailTestFramework/src/Framework/Alstom.Spectrail.TestFramework/Interfaces/IHandlerFactory.cs
+++ Alstom/SpectrailTestFramework/src/Framework/Alstom.Spectrail.TestFramework/Interfaces/IHandlerFactory.cs
@@ -1,4 +1,28 @@
-﻿namespace Alstom.Spectrail.TestFramework.Interfaces;
+﻿#region ©COPYRIGHT
+
+// /*******************************************************************************
+//  *   © COPYRIGHT ALSTOM SA 2025 - ALL RIGHTS RESERVED
+//  *
+//  *  This file is part of the Alstom Spectrail project.
+//  *  Unauthorized copying, modification, distribution, or use of this file,
+//  *  via any medium, is strictly prohibited.
+//  *
+//  *  Proprietary and confidential.
+//  *  Created and maintained by Alstom for internal use in the Spectrail project.
+//  ******************************************************************************/
+// 
+//  /*******************************************************************************
+// AuthorName: SUBHASISH BISWAS
+// Email: subhasish.biswas@alstomgroup.com
+// FileName: IHandlerFactory.cs
+// ProjectName: Alstom.Spectrail.TestFramework
+// Created by SUBHASISH BISWAS On: 2025-03-13
+// Updated by SUBHASISH BISWAS On: 2025-03-13
+//  ******************************************************************************/
+
+#endregion
+
+namespace Alstom.Spectrail.TestFramework.Interfaces;
 
 public interface IHandlerFactory
 {
