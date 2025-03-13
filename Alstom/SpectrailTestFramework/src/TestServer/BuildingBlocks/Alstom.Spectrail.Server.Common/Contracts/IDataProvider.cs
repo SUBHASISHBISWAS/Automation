559c5d36d04c277210fdd3d46ae1d553360d4fd8
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-03-13 20:04:25 +0530

    Add copyright headers to all project files
    Included copyright notices indicating proprietary rights, authorship, and confidentiality for all relevant files in the Alstom Spectrail project. This ensures compliance with intellectual property and confidentiality policies.

--- Alstom/SpectrailTestFramework/src/TestServer/BuildingBlocks/Alstom.Spectrail.Server.Common/Contracts/IDataProvider.cs
+++ Alstom/SpectrailTestFramework/src/TestServer/BuildingBlocks/Alstom.Spectrail.Server.Common/Contracts/IDataProvider.cs
@@ -1,3 +1,27 @@
+#region ©COPYRIGHT
+
+// /*******************************************************************************
+//  *   © COPYRIGHT ALSTOM SA 2025 - ALL RIGHTS RESERVED
+//  *
+//  *  This file is part of the Alstom Spectrail project.
+//  *  Unauthorized copying, modification, distribution, or use of this file,
+//  *  via any medium, is strictly prohibited.
+//  *
+//  *  Proprietary and confidential.
+//  *  Created and maintained by Alstom for internal use in the Spectrail project.
+//  ******************************************************************************/
+// 
+//  /*******************************************************************************
+// AuthorName: SUBHASISH BISWAS
+// Email: subhasish.biswas@alstomgroup.com
+// FileName: IDataProvider.cs
+// ProjectName: Alstom.Spectrail.Server.Common
+// Created by SUBHASISH BISWAS On: 2025-03-04
+// Updated by SUBHASISH BISWAS On: 2025-03-13
+//  ******************************************************************************/
+
+#endregion
+
 #region
 
 using System.Linq.Expressions;
