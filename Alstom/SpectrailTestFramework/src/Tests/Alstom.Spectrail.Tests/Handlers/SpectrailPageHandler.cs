973db22cd0fa25169aa7e286762f031259d0c561
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-03-13 20:05:55 +0530

    Add copyright headers to all project files
    Introduced standardized copyright headers across all files in the `Alstom.Spectrail.Tests` project. This ensures proper attribution, legal documentation, and compliance with proprietary and confidentiality standards. Updated metadata includes author details and timestamps.

--- Alstom/SpectrailTestFramework/src/Tests/Alstom.Spectrail.Tests/Handlers/SpectrailPageHandler.cs
+++ Alstom/SpectrailTestFramework/src/Tests/Alstom.Spectrail.Tests/Handlers/SpectrailPageHandler.cs
@@ -1,4 +1,28 @@
-﻿#region
+﻿#region ©COPYRIGHT
+
+// /*******************************************************************************
+//  *   © COPYRIGHT ALSTOM SA 2025 - ALL RIGHTS RESERVED
+//  *
+//  *  This file is part of the Alstom Spectrail project.
+//  *  Unauthorized copying, modification, distribution, or use of this file,
+//  *  via any medium, is strictly prohibited.
+//  *
+//  *  Proprietary and confidential.
+//  *  Created and maintained by Alstom for internal use in the Spectrail project.
+//  ******************************************************************************/
+// 
+//  /*******************************************************************************
+// AuthorName: SUBHASISH BISWAS
+// Email: subhasish.biswas@alstomgroup.com
+// FileName: SpectrailPageHandler.cs
+// ProjectName: Alstom.Spectrail.Tests
+// Created by SUBHASISH BISWAS On: 2025-03-13
+// Updated by SUBHASISH BISWAS On: 2025-03-13
+//  ******************************************************************************/
+
+#endregion
+
+#region
 
 using Alstom.Spectrail.TestFramework.Actions;
 using Alstom.Spectrail.TestFramework.Attributes;
--- Alstom/SpectrailTestFramework/src/Tests/Alstom.Spectrail.Tests/MSTestSettings.cs
+++ Alstom/SpectrailTestFramework/src/Tests/Alstom.Spectrail.Tests/MSTestSettings.cs
@@ -1,1 +1,25 @@
-﻿[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+﻿#region ©COPYRIGHT
+
+// /*******************************************************************************
+//  *   © COPYRIGHT ALSTOM SA 2025 - ALL RIGHTS RESERVED
+//  *
+//  *  This file is part of the Alstom Spectrail project.
+//  *  Unauthorized copying, modification, distribution, or use of this file,
+//  *  via any medium, is strictly prohibited.
+//  *
+//  *  Proprietary and confidential.
+//  *  Created and maintained by Alstom for internal use in the Spectrail project.
+//  ******************************************************************************/
+// 
+//  /*******************************************************************************
+// AuthorName: SUBHASISH BISWAS
+// Email: subhasish.biswas@alstomgroup.com
+// FileName: MSTestSettings.cs
+// ProjectName: Alstom.Spectrail.Tests
+// Created by SUBHASISH BISWAS On: 2025-02-19
+// Updated by SUBHASISH BISWAS On: 2025-03-13
+//  ******************************************************************************/
+
+#endregion
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
