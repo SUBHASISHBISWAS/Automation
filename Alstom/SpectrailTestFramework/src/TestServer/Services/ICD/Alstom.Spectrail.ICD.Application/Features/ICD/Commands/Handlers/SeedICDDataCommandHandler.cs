3f42a30bd394e06e2083045807c92ac9459a5355
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-03-13 20:02:54 +0530

    Add copyright notice and file metadata to multiple files
    Included copyright headers and metadata across various application files. This ensures clarity on file ownership, authorship, and usage limitations for the Alstom Spectrail project.

--- Alstom/SpectrailTestFramework/src/TestServer/Services/ICD/Alstom.Spectrail.ICD.Application/Features/ICD/Commands/Handlers/SeedICDDataCommandHandler.cs
+++ Alstom/SpectrailTestFramework/src/TestServer/Services/ICD/Alstom.Spectrail.ICD.Application/Features/ICD/Commands/Handlers/SeedICDDataCommandHandler.cs
@@ -1,3 +1,27 @@
+#region ©COPYRIGHT
+
+// /*******************************************************************************
+//  *   © COPYRIGHT ALSTOM SA 2025 - ALL RIGHTS RESERVED
+//  *
+//  *  This file is part of the Alstom Spectrail project.
+//  *  Unauthorized copying, modification, distribution, or use of this file,
+//  *  via any medium, is strictly prohibited.
+//  *
+//  *  Proprietary and confidential.
+//  *  Created and maintained by Alstom for internal use in the Spectrail project.
+//  ******************************************************************************/
+// 
+//  /*******************************************************************************
+// AuthorName: SUBHASISH BISWAS
+// Email: subhasish.biswas@alstomgroup.com
+// FileName: SeedICDDataCommandHandler.cs
+// ProjectName: Alstom.Spectrail.ICD.Application
+// Created by SUBHASISH BISWAS On: 2025-03-12
+// Updated by SUBHASISH BISWAS On: 2025-03-13
+//  ******************************************************************************/
+
+#endregion
+
 #region
 
 using Alstom.Spectrail.ICD.Application.Contracts;
